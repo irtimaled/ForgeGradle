8943970dc9eda13fa598af1c87a609848ca30bfc
Irtimaled <irtimaled@gmail.com>
2019-01-30 14:58:03 -0800

    Use minecraft version specific mappings

--- src/main/java/net/minecraftforge/gradle/common/BasePlugin.java
+++ src/main/java/net/minecraftforge/gradle/common/BasePlugin.java
@@ -234,7 +234,7 @@ public abstract class BasePlugin<K extends BaseExtension> implements Plugin<Proj
         project.getDependencies().add(CONFIG_MCP_DATA, ImmutableMap.of(
                 "group", "com.github.Irtimaled",
                 "name", "MCPConfig",
-                "version", "jitpack-SNAPSHOT",
+                "version", "1.13.1-SNAPSHOT",
                 "ext", "zip"
                 ));
 
